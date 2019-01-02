ffca80f6b26ebdf1aa583f8992b616b6ad44552e
Silviu Troscot <silviu.troscot@student.manchester.ac.uk>
2019-01-02 03:57:32 -0800

    implemented db connection and username validation

--- blackjack/api.py
+++ blackjack/api.py
@@ -1,56 +1,59 @@
-from enum import Enum
-import random
+import mysql.connector
+import re
 
-class SuitSymbols(Enum):
-    club = "club"
-    diamond = "diamond"
-    heart = "heart"
-    spade = "spade"
+sqlConnector = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="parolaST1"
+)
 
-class Values(Enum):
-    one = 1
-    two = 2
-    three = 3
-    four = 4
-    five = 5
-    six = 6
-    seven = 7
-    eight = 8
-    nine = 9
-    ten = 10
-    twelve = 12
-    thirteen = 13
-    fourteen = 14
+# create the database and tables if not existent
+def initDatabase():
+    # create a database for the players if not existent 
+    cursor = sqlConnector.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXISTS users")
+
+    # create table for storing users data, consisting of:
+    # username, salt, passhash, amount of money available
+    try:
+        usersDbConnector = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            passwd="parolaST1",
+            database="users"
+        )
+    except:
+        print("database connection failed")
+
+    usersDbCursor = usersDbConnector.cursor()
+    usersDbCursor.execute("CREATE TABLE IF NOT EXISTS logins " +
+        "(username VARCHAR(255) PRIMARY KEY, passhash VARCHAR(255), " +
+        "salt VARCHAR(255), credit BIGINT)")
 
 
-# class representing a card from the deck
-# each card has a value (e.g. 1, 2,..., 13(for Q), 14(for K))
-# and a colour
-class Card:
-    def __init__(self, value, suit_symbol):
-        self.value = value
-        self.suit_symbol = suit_symbol
+# insert an user in the database and provides them the initial amount
+# of money to start the game.
+# return true if the user is inserted successfully and false otherwise
+def registerUser(usersDbCursor, username, password):
+    validateUsername(usersDbCursor, username)
 
-# class representing a standard 52 cards deck
-# where each value occurs four times, one for each suit symbol
-
-class Deck:
-    def __init__(self):
-        self.cards = []
-        for value in Values:
-            for suitSymbol in SuitSymbols:
-                card = Card(value, suitSymbol)
-                self.cards.append(card)
-
-        self.shuffleDeck()
-
-    def shuffleDeck(self):
-        random.shuffle(self.cards)
-
-    # return the next card in the deck and remove it     
-    def getNextCard(self):
-        deckSize = len(self.cards)
-        print (deckSize)
-        nextCard = self.cards[deckSize - 1]
-        del self.cards[deckSize - 1]
-        return nextCard
+# check if the username contains only letters and digits and it is 
+# shorter than 255 chars and it is not already existent in the database
+def validateUsername(username):
+    if len(username) < 255:
+        if re.match("^[A-Za-z0-9_-]*$", username):
+            usersDbConnector = mysql.connector.connect(
+                host="localhost",
+                user="root",
+                passwd="parolaST1",
+                database="users"
+                )
+            usersDbCursor = usersDbConnector.cursor()
+            selectUserStatement = "SELECT * FROM logins WHERE username = %s"
+            adr = (username, )
+            usersDbCursor.execute(selectUserStatement, adr)
+            result = usersDbCursor.fetchall()
+            if len(result) is 0:
+                return True
+    
+    return False
---
+++ blackjack/gameUtils.py
@@ -0,0 +1,60 @@
+from enum import Enum
+import random
+
+# enum representing the 4 possible symbols of a card
+class SuitSymbols(Enum):
+    club = "club"
+    diamond = "diamond"
+    heart = "heart"
+    spade = "spade"
+
+# enum for card values; for the purpose of this game
+# the logic of handling the value of ace (1/11) is handled
+# in the game engine
+class Values(Enum):
+    one = 1
+    two = 2
+    three = 3
+    four = 4
+    five = 5
+    six = 6
+    seven = 7
+    eight = 8
+    nine = 9
+    ten = 10
+    twelve = 12
+    thirteen = 13
+    fourteen = 14
+
+
+# class representing a card from the deck
+# each card has a value (e.g. 1, 2,..., 13(for Q), 14(for K))
+# and a colour
+class Card:
+    def __init__(self, value, suitSymbol):
+        self.value = value
+        self.suit_symbol = suitSymbol
+
+# class representing a standard 52 cards deck
+# where each value occurs four times, one for each suit symbol
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+        for value in Values:
+            for suitSymbol in SuitSymbols:
+                card = Card(value, suitSymbol)
+                self.cards.append(card)
+
+        self.shuffleDeck()
+
+    def shuffleDeck(self):
+        random.shuffle(self.cards)
+
+    # return the next card in the deck and remove it     
+    def getNextCard(self):
+        deckSize = len(self.cards)
+        print (deckSize)
+        nextCard = self.cards[deckSize - 1]
+        del self.cards[deckSize - 1]
+        return nextCard
