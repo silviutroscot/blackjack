d7b6a521530314fb4fb2c6de2b0f1255b5d06132
Silviu Troscot <silviu.troscot@student.manchester.ac.uk>
2019-01-05 00:31:08 +0200

    player sum logic prototype

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -16,11 +16,13 @@ def initGame():
     else:
         print("Sorry, you typed an invalid character \n")
 
+# interface for registering a new player
 def registerUser():
     username = input("Please insert your username:")
     password = getpass.getpass("Password: ")
     print(api.registerUser(username, password))
 
+# interface for authenticating a player
 def login():
     username = input("Please insert your username: ")
     password = getpass.getpass("Password: ")
@@ -30,12 +32,37 @@ def login():
         if (credit > 0):
             print ('''Welcome {username}, your current credit is {credit}\
             '''.format(username=username, credit=credit))
+            startGameRound(username, credit)
+        else: 
+            print("You have insufficient founds; please top up your account")
+
+# one round of the game
+def startGameRound(username, credit): 
+    # TODO: error handling
+    bet = int(input("Please insert your bet (numerical value): "))
+    if bet > credit: 
+        print("You cannot bet more than your current sold; please try again")
+        startGameRound(username, credit) 
+    else: 
+        deck = gameUtils.Deck() 
+        dealer = [deck.getNextCard().value.value]
+        dealer.append(deck.getNextCard().value.value) 
+        hand = (deck.getNextCard().value.value, deck.getNextCard().value.value)
+        sum = playerSum(hand, 0)
+
+# handler for the gameplay of a player in one game round
+def playerSum(hand, noOfAces): 
+    playerAces = hand.count(1)
+    # compute the sum for each ace as having value 11, and decrease the value to 1, one by one, until the sum is smaller than 21 
+    sum = 0 
+    for card in hand: 
+        sum += card
+    return sum
 
 
-if __name__ == "__main__":
-    api.initDatabase()
+if __name__ == "__main__": 
+    api.initDatabase() 
     deck = gameUtils.Deck()
-    print(deck.getNextCard().value.value)
+    print(deck.getNextCard().value.value) 
     initGame()
     #print(api.login("silviu", "parola1"))
-        
